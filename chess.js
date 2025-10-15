fa468974fdd90db393fd5d23b011de6db3f99dde
frowningfrog <natalizedstolpinator@hotmail.com>
2025-10-15 12:31:48 -0600

    add png

--- chess.js
+++ chess.js
@@ -1,21 +1,43 @@
-var intro = document.getElementById("intro");
-var dropbtn = document.getElementById("dropbtn");
+const intro = document.getElementById("intro");
+const dropbtn = document.getElementById("dropbtn");
+const kingb = document.getElementById("kb");
+const queenb = document.getElementById("qb");
+const pawnb = document.getElementById("pb");
 
 function king() {
     dropbtn.innerHTML = "The King";
-    document.getElementById("kb").style.display = "none";
-    document.getElementById("qb").style.display = "block";
+    kingb.style.display = "none";
+    queenb.style.display = "block";
+    pawnb.style.display = "block";
     intro.style.display = "none";
+    document.getElementById("queenp").style.display = "none";
+    document.getElementById("pawnp").style.display = "none";
     document.getElementById("queen").style.display = "none";
+    document.getElementById("kingp").style.display = "block";
     document.getElementById("king").style.display = "block";
     document.getElementById("nav").style.display = "none";
 }
 
 function queen() {
     dropbtn.innerHTML = "The Queen";
-    document.getElementById("qb").style.display = "none";
-    document.getElementById("kb").style.display = "block";
+    queenb.style.display = "none";
+    kingb.style.display = "block";
+    pawnb.style.display = "block";
     intro.style.display = "none";
+    document.getElementById("kingp").style.display = "none";
+    document.getElementById("pawnp").style.display = "none";
+    document.getElementById("queenp").style.display = "block";
     document.getElementById("king").style.display = "none";
     document.getElementById("queen").style.display = "block";
+}
+
+function pawn() {
+    dropbtn.innerHTML = "The Pawn";
+    pawnb.style.display = "none";
+    kingb.style.display = "block";
+    queenb.style.display = "block";
+    intro.style.display = "none";
+    document.getElementById("kingp").style.display = "none";
+    document.getElementById("queenp").style.display = "none";
+    document.getElementById("pawnp").style.display = "block";
 }
